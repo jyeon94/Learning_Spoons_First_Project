f52fe803e3e20e2809f00dc8efabaf0488928135
seungjae1162 <59638478+seungjae1162@users.noreply.github.com>
2020-02-04 20:59:01 +0900

    Update abandoned_animal_grouped_by_year-kind.R

--- abandoned_animal_grouped_by_year-kind.R
+++ abandoned_animal_grouped_by_year-kind.R
@@ -1,9 +1,10 @@
+install.packages("tidyverse")
 library(tidyverse)
 library(lubridate)
-df <- read.csv('csv/abandoned_animal(All).csv', fileEncoding="UTF-8")
 
+df <- read.csv("C:\\Users\\seung\\Desktop\\2020년도\\NanoD\\github\\Learning_Spoons_First_Project\\csv\\abandoned_animal(All).csv",fileEncoding='UTF-8')
 df2<-df %>%
-  filter(종류=='개')%>%
+  filter(종류=='개') %>%
   group_by(year(접수일),종류) %>%
   summarise(count=n())
 
